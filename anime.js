c5d20a1ed5acc501b1c12427e6772c5b87d6fdf6
Jacob Allen <boat.mon@gmail.com>
2020-01-29 16:23:05 -0500

    redoing the nav

--- anime.js
+++ anime.js
@@ -67,5 +67,15 @@ function pageLoaded(){
 
 }
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 
 window.addEventListener("load", pageLoaded);
