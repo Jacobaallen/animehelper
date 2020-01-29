674772cead964b9c8e5b5d8814cdb79237c81381
Jacob Allen <boat.mon@gmail.com>
2020-01-29 16:35:42 -0500

    updating the nav

--- News.js
+++ News.js
@@ -45,3 +45,13 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
