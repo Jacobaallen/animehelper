82047fa24cf90f0a4661d217d7bf80b5c41e4ac8
Jacob Allen <boat.mon@gmail.com>
2020-04-13 13:19:20 -0400

    new folder to hold all game files and images

---
+++ Games/whackaluffy.js
@@ -0,0 +1,45 @@
+const luffy = document.querySelectorAll('.luffy')
+const scoreBoard    = document.querySelector('#score')
+let remainingPeeps, score
+
+function randomLuffy() {
+  const index = Math.floor(Math.random() * luffy.length)
+  return luffy[index]
+}
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function sprout(luffy){
+  luffy.classList.add('up')
+}
+
+function shrink(luffy){
+  luffy.classList.remove('up')
+}
+
+function bonk(luffy) {
+  shrink(luffy)
+  score++
+  scoreBoard.textContent = score
+}
+
+function peep(luffy) {
+  const time = randomTime(200, 2000)
+  sprout(luffy)
+  setTimeout( () => { shrink(luffy) }, time)
+  if(remainingPeeps > 0){
+    remainingPeeps--
+    const luffy = randomLuffy()
+    setTimeout( () => { peep(luffy) }, time )
+  }
+}
+
+function startGame() {
+  remainingPeeps = 20
+  score = 0
+  scoreBoard.textContent = score
+  const luffy = randomLuffy()
+  peep(luffy)
+}
